86913eab59582e0f3984745e82a0e3d31071d35a
matheusboliveir <theus.braz18@gmail.com>
2022-05-03 17:52:18 -0300

    Criação das diferentes telas do popup - aula 02 NLW Return

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ module.exports = {
     extend: {
       colors: {
         brand: {
-          500: '#8257E6'
-        }
+          500: '#8257E6',
+          300: '#996DFF',
+        },
+      },
+      borderRadius: {
+        md: '4px',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
